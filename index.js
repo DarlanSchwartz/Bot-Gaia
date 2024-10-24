9a3876c9088d0bcfb86bca26732fef76dcf842f2
Schwartz <darlanswtz@gmail.com>
2024-10-23 21:09:50 -0300

    Feat:added gemidão

--- index.js
+++ index.js
@@ -1,108 +1,68 @@
-const express = require('express');
-// const TelegramBot = require('node-telegram-bot-api');
-const { Provider } = require("./axios");
-const PORT = process.env.PORT || 4040;
-
-const app = express();
-app.use(express.json());
-// const TOKEN = "7728375831:AAEAlHQP4JgvSw0dnBSJ3Zseta6NAjTc728";
-// const bot = new TelegramBot(TOKEN, { polling: true });
-
+const fs = require('fs');
+const TelegramBot = require('node-telegram-bot-api');
+const TOKEN = "7728375831:AAEAlHQP4JgvSw0dnBSJ3Zseta6NAjTc728";
+const bot = new TelegramBot(TOKEN, { polling: true });
 
 const Messages = {
     greetings: `
         Olá, Wellington, tudo bem? 
-        Acabei de ver aqui que você tem um cadastro de propriedade comigo ☺
-        Estou vendo que essa é a primeira vez usando minha tecnologia, então vou lhe explicar como funciono.
+
+Acabei de ver aqui que você tem um cadastro de propriedade comigo ☺
+
+Estou vendo que essa é a primeira vez usando minha tecnologia, então vou lhe explicar como funciono.
     `,
     afterGreetings: `
         Meu nome é Gaia, uma inteligência Artificial criada para auxiliar agricultores profissionalizar em sua produção de forma sustentável e que maxime a geração de renda. 
-        Possuo alguns serviços que você pode utilizar, como você já está cadastrado convosco, você já pode solicitar um série de Serviços.
+
+Possuo alguns serviços que você pode utilizar, como você já está cadastrado convosco, você já pode solicitar um série de Serviços.
     `
 };
 
+const options = [
+    {
+        text: "Análise da saúde da plantação",
+    },
+    {
+        text: "Análise meteorologia por inteligência Artificial",
+    },
+    {
+        text: "Conselho de boa práticas que fortaleçam a Bioecononia sustentável",
+    },
+    {
+        text: "Serviços completo de análise da propriedade, com dados via satélite",
+    },
+    {
+        text: "Não desejo realizar serviço, quero continuar a conversa",
+    }
+];
 
-async function sendMessage(chatId, text) {
-    return Provider.post("/sendMessage", {
-        chat_id: chatId,
-        text: text
-    });
-}
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
-async function sendPool(chatId, question, options) {
-    try {
-        await Provider.post("/sendPoll", {
-            chat_id: chatId,
-            question,
-            allows_multiple_answers: false,
-            options: [
-                {
-                    text: "Análise da saúde da plantação",
-                },
-                {
-                    text: "Análise meteorologia por inteligência Artificial",
-                },
-                {
-                    text: "Conselho de boa práticas que fortaleçam a Bioecononia sustentável",
-                },
-                {
-                    text: "Serviços completo de análise da propriedade, com dados via satélite",
-                },
-                {
-                    text: "Não desejo realizar serviço, quero continuar a conversa",
-                }
-            ]
+    if (msg?.voice) {
+        const file = fs.readFileSync("./file.pdf");
+        const audio = fs.readFileSync("./zap.mp3");
+        bot.sendMessage(chatId, "Aguarde... Estou processando o arquivo");
+        bot.sendVoice(chatId, audio, { caption: "Olá, vejo..." }, {
+            filename: "audio",
+            contentType: "audio/mp3"
         });
-    } catch (error) {
-        console.log(error.response.data);
+        return bot.sendDocument(chatId, file);
     }
-}
 
-async function sendFile(chatId, file) {
-    try {
-        await bot.sendDocument(chatId, "./file.pdf");
-    } catch (error) {
-        console.log(error.response);
-    }
-}
-
-function handleMessage(message) {
-    try {
-        if (message?.voice) {
-            return sendFile(message.chat.id, file);
-        }
-        const chatId = message.chat.id;
-        const text = message.text;
-        if (text.toLowerCase().includes("olá") || text.toLowerCase().includes("oi") || text.toLowerCase().includes("hello") || text.toLowerCase().includes("hi") || text.toLowerCase().includes("hey")) {
+    if (text.toLowerCase().includes("olá") || text.toLowerCase().includes("oi") || text.toLowerCase().includes("hello") || text.toLowerCase().includes("hi") || text.toLowerCase().includes("hey")) {
+        setTimeout(() => {
+            bot.sendMessage(chatId, Messages.afterGreetings);
             setTimeout(() => {
-                sendMessage(chatId, Messages.afterGreetings);
-                setTimeout(() => {
-                    sendPool(chatId, "Você tem interesse em realizar algum dos serviços abaixo: ");
-                }, 2000);
+                bot.sendPoll(chatId, "Você tem interesse em realizar algum dos serviços abaixo: ", options, {
+                    allows_multiple_answers: false
+                });
             }, 2000);
+        }, 2000);
 
-            return sendMessage(chatId, Messages.greetings);
-        }
-
-        return sendMessage(chatId, text);
-    } catch (error) {
-
+        return bot.sendMessage(chatId, Messages.greetings);
     }
-}
 
-app.post("*", async (req, res) => {
-    console.log(req.body.message);
-    await handleMessage(req.body.message);
-    return res.send("Hello post");
-});
-app.get("*", async (req, res) => {
-    return res.send("Hello get");
-});
-
-app.listen(PORT, (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(`Server is running on port ${PORT}`);
-    }
+    bot.sendMessage(chatId, 'Received your message');
 });
